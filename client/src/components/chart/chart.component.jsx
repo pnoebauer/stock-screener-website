d55dda589cfa1df6f4e5523e55e120a4294f23b0
Philip Noebauer <pnoebauer>
2021-07-04 08:50:40 +0800

    work on chart component

--- client/src/components/chart/chart.component.jsx
+++ client/src/components/chart/chart.component.jsx
@@ -33,6 +33,9 @@ import {
 import {ema, sma, macd} from 'react-stockcharts/lib/indicator';
 import {fitWidth} from 'react-stockcharts/lib/helper';
 
+import Modal from '../portal-modal/modal.component';
+import ChartIndicatorConfigurationForm from '../chart-indicator-config/chart-indicator-config.component';
+
 function getMaxUndefined(calculators) {
 	return calculators.map(each => each.undefinedLength()).reduce((a, b) => Math.max(a, b));
 }
@@ -92,6 +95,8 @@ class CandleStickChartPanToLoadMore extends React.Component {
 			})
 			.accessor(d => d.smaVolume50);
 
+		const indicators = {ema26, ema12, macdCalculator, smaVolume50};
+
 		const maxWindowSize = getMaxUndefined([ema26, ema12, macdCalculator, smaVolume50]);
 		/* SERVER - START */
 		const dataToCalculate = inputData.slice(-LENGTH_TO_SHOW - maxWindowSize);
@@ -115,23 +120,35 @@ class CandleStickChartPanToLoadMore extends React.Component {
 		// console.log(linearData.length)
 
 		this.state = {
-			ema26,
-			ema12,
-			macdCalculator,
-			smaVolume50,
-			linearData,
+			...indicators,
+			// linearData, //-->appears not to be needed
 			data: linearData,
 			xScale,
 			xAccessor,
 			displayXAccessor,
+			visible: false,
+			inactive: false,
 		};
 
-		// console.log('s', this.state);
+		// this.state = {
+		// 	ema26,
+		// 	ema12,
+		// 	macdCalculator,
+		// 	smaVolume50,
+		// 	// linearData, //-->appears not to be needed
+		// 	data: linearData,
+		// 	xScale,
+		// 	xAccessor,
+		// 	displayXAccessor,
+		// };
+
+		console.log('s', this.state);
 		// this.handleDownloadMore = this.handleDownloadMore.bind(this);
 	}
 
 	// handleDownloadMore(start, end) {
 	handleDownloadMore = (start, end) => {
+		console.log(start, end, 'start, end');
 		if (Math.ceil(start) === end) return;
 		// console.log("rows to download", rowsToDownload, start, end)
 		const {data: prevData, ema26, ema12, macdCalculator, smaVolume50} = this.state;
@@ -181,6 +198,14 @@ class CandleStickChartPanToLoadMore extends React.Component {
 		}, 300);
 	};
 
+	show = () => {
+		this.setState({visible: true, inactive: false});
+	};
+
+	hide = () => {
+		this.setState({visible: false, inactive: false});
+	};
+
 	render() {
 		const {type, width, ratio, height} = this.props;
 
@@ -200,136 +225,158 @@ class CandleStickChartPanToLoadMore extends React.Component {
 		const {data: inputData} = this.props;
 
 		return (
-			<ChartCanvas
-				ratio={ratio}
-				width={width}
-				height={600}
-				margin={{left: 70, right: 70, top: 20, bottom: 30}}
-				type={type}
-				seriesName='MSFT'
-				data={data}
-				xScale={xScale}
-				xAccessor={xAccessor}
-				displayXAccessor={displayXAccessor}
-				onLoadMore={this.handleDownloadMore}
-			>
-				<Chart
-					id={1}
-					height={400}
-					yExtents={[d => [d.high, d.low], ema26.accessor(), ema12.accessor()]}
-					padding={{top: 10, bottom: 20}}
+			<>
+				<ChartCanvas
+					ratio={ratio}
+					width={width}
+					height={600}
+					margin={{left: 70, right: 70, top: 20, bottom: 30}}
+					type={type}
+					seriesName='MSFT'
+					data={data}
+					xScale={xScale}
+					xAccessor={xAccessor}
+					displayXAccessor={displayXAccessor}
+					onLoadMore={this.handleDownloadMore}
 				>
-					{/* <XAxis axisAt='bottom' orient='bottom' showTicks={false} outerTickSize={0} />
+					<Chart
+						id={1}
+						height={400}
+						yExtents={[d => [d.high, d.low], ema26.accessor(), ema12.accessor()]}
+						padding={{top: 10, bottom: 20}}
+					>
+						{/* <XAxis axisAt='bottom' orient='bottom' showTicks={false} outerTickSize={0} />
 					<YAxis axisAt='right' orient='right' ticks={5} /> */}
-					<XAxis axisAt='bottom' orient='bottom' />
-					<YAxis axisAt='left' orient='left' ticks={5} />
+						<XAxis axisAt='bottom' orient='bottom' />
+						<YAxis axisAt='left' orient='left' ticks={5} />
 
-					<MouseCoordinateY at='left' orient='left' displayFormat={format('.2f')} />
-					<MouseCoordinateX
-						at='bottom'
-						orient='bottom'
-						displayFormat={timeFormat('%Y-%m-%d')}
-					/>
+						<MouseCoordinateY at='left' orient='left' displayFormat={format('.2f')} />
+						<MouseCoordinateX
+							at='bottom'
+							orient='bottom'
+							displayFormat={timeFormat('%Y-%m-%d')}
+						/>
 
-					<CandlestickSeries />
-					<LineSeries yAccessor={ema26.accessor()} stroke={ema26.stroke()} />
-					<LineSeries yAccessor={ema12.accessor()} stroke={ema12.stroke()} />
+						<CandlestickSeries />
+						<LineSeries yAccessor={ema26.accessor()} stroke={ema26.stroke()} />
+						<LineSeries yAccessor={ema12.accessor()} stroke={ema12.stroke()} />
 
-					<CurrentCoordinate yAccessor={ema26.accessor()} fill={ema26.stroke()} />
-					<CurrentCoordinate yAccessor={ema12.accessor()} fill={ema12.stroke()} />
+						<CurrentCoordinate yAccessor={ema26.accessor()} fill={ema26.stroke()} />
+						<CurrentCoordinate yAccessor={ema12.accessor()} fill={ema12.stroke()} />
 
-					<EdgeIndicator
-						itemType='last'
-						orient='right'
-						edgeAt='right'
-						yAccessor={d => d.close}
-						fill={d => (d.close > d.open ? '#6BA583' : '#FF0000')}
-					/>
+						<EdgeIndicator
+							itemType='last'
+							orient='right'
+							edgeAt='right'
+							yAccessor={d => d.close}
+							fill={d => (d.close > d.open ? '#6BA583' : '#FF0000')}
+						/>
 
-					<OHLCTooltip origin={[-40, 0]} />
-					<MovingAverageTooltip
-						// onClick={e => console.log(e)}
-						onClick={e => {
-							// ema26.options({windowSize: 50});
-							const ema26 = ema()
-								.id(0)
-								.options({windowSize: 1})
-								.merge((d, c) => {
-									// console.log(
-									// 	'CLICKED',
-									// 	'c=ema26',
-									// 	c,
-									// 	'd=the full candle incl. indicators',
-									// 	d,
-									// 	'dc'
-									// );
-									d.ema26 = c;
-								})
-								.accessor(d => d.ema26);
+						<OHLCTooltip origin={[-40, 0]} />
+						<MovingAverageTooltip
+							// onClick={e => console.log(e)}
+							onClick={e => {
+								this.show();
 
-							const maxWindowSize = getMaxUndefined([ema26]);
-							console.log('MAX-----', maxWindowSize);
-							/* SERVER - START */
-							const dataToCalculate = inputData.slice(-LENGTH_TO_SHOW - maxWindowSize);
+								console.log(e, 'e');
+								// console.log(this.state.ema12.id(), this.state.ema12.options(), 'id');
+								// let indicatorName;
+								// Object.keys(this.state).some(key => {
+								// 	try {
+								// 		console.log(key, this.state[key].id(), e.chartId, 'ci');
 
-							const calculatedData = ema26(dataToCalculate);
-							console.log('calculatedData-----', calculatedData);
-							const indexCalculator =
-								discontinuousTimeScaleProviderBuilder().indexCalculator();
+								// 		if (this.state[key].id() === e.chartId) {
+								// 			indicatorName = key;
+								// 			return true;
+								// 		}
+								// 	} catch (e) {
+								// 		//not an indicator
+								// 	}
+								// });
 
-							// console.log(inputData.length, dataToCalculate.length, maxWindowSize)
-							const {index} = indexCalculator(calculatedData);
-							/* SERVER - END */
+								const {indicatorName} = e;
+								console.log(indicatorName, 'indicatorName');
 
-							const xScaleProvider =
-								discontinuousTimeScaleProviderBuilder().withIndex(index);
-							const {
-								data: linearData,
-								xScale,
-								xAccessor,
-								displayXAccessor,
-							} = xScaleProvider(calculatedData.slice(-LENGTH_TO_SHOW));
+								// ema26.options({windowSize: 50});
+								// const ema26 = ema()
+								// 	.id(0)
+								// 	.options({windowSize: 1, sourcePath: 'high'})
+								// 	.merge((d, c) => {
+								// 		d.ema26 = c;
+								// 	})
+								// 	.accessor(d => d.ema26);
+								// const ema26 = this.state.ema26.options({windowSize: 1, sourcePath: 'high'});
+								// console.log(ema26.merge(), 'ac');
 
-							// console.log(head(linearData), last(linearData))
-							// console.log(linearData.length)
+								const updatedIndicator = this.state[indicatorName].options({
+									windowSize: 1,
+									sourcePath: 'high',
+								});
 
-							// this.state = {
-							// 	ema26,
-							// 	linearData,
-							// 	data: linearData,
-							// 	xScale,
-							// 	xAccessor,
-							// 	displayXAccessor,
-							// };
+								// const maxWindowSize = getMaxUndefined([ema26]);
 
-							this.setState({
-								ema26,
-								data: linearData,
-								xScale,
-								xAccessor,
-								displayXAccessor,
-							});
+								const maxWindowSize = getMaxUndefined([updatedIndicator]);
+								// console.log('MAX-----', maxWindowSize);
+								/* SERVER - START */
+								const dataToCalculate = inputData.slice(-LENGTH_TO_SHOW - maxWindowSize);
 
-							console.log(e);
-						}}
-						origin={[-38, 15]}
-						options={[
-							{
-								yAccessor: ema26.accessor(),
-								type: ema26.type(),
-								stroke: ema26.stroke(),
-								...ema26.options(),
-							},
-							{
-								yAccessor: ema12.accessor(),
-								type: ema12.type(),
-								stroke: ema12.stroke(),
-								...ema12.options(),
-							},
-						]}
-					/>
-				</Chart>
-				{/* 
+								// const calculatedData = ema26(dataToCalculate);
+								const calculatedData = updatedIndicator(dataToCalculate);
+								// console.log('calculatedData-----', calculatedData);
+								const indexCalculator =
+									discontinuousTimeScaleProviderBuilder().indexCalculator();
+
+								// console.log(inputData.length, dataToCalculate.length, maxWindowSize)
+								const {index} = indexCalculator(calculatedData);
+								/* SERVER - END */
+
+								const xScaleProvider =
+									discontinuousTimeScaleProviderBuilder().withIndex(index);
+								const {
+									data: linearData,
+									xScale,
+									xAccessor,
+									displayXAccessor,
+								} = xScaleProvider(calculatedData.slice(-LENGTH_TO_SHOW));
+
+								// console.log(head(linearData), last(linearData))
+								// console.log(linearData.length)
+
+								// this.setState({
+								// 	ema26,
+								// 	data: linearData,
+								// 	xScale,
+								// 	xAccessor,
+								// 	displayXAccessor,
+								// });
+								this.setState({
+									updatedIndicator,
+									data: linearData,
+									xScale,
+									xAccessor,
+									displayXAccessor,
+								});
+							}}
+							origin={[-38, 15]}
+							options={[
+								{
+									yAccessor: ema26.accessor(),
+									type: ema26.type(),
+									stroke: ema26.stroke(),
+									...ema26.options(),
+									indicatorName: 'ema26',
+								},
+								{
+									yAccessor: ema12.accessor(),
+									type: ema12.type(),
+									stroke: ema12.stroke(),
+									...ema12.options(),
+									indicatorName: 'ema12',
+								},
+							]}
+						/>
+					</Chart>
+					{/* 
 				<Chart
 					id={2}
 					height={150}
@@ -377,8 +424,19 @@ class CandleStickChartPanToLoadMore extends React.Component {
 					/>
 				</Chart>
 				*/}
-				<CrossHairCursor />
-			</ChartCanvas>
+					<CrossHairCursor />
+				</ChartCanvas>
+
+				{this.state.visible ? (
+					<Modal
+						inactive={this.state.inactive}
+						style={{height: '60%', width: '50%'}}
+						onClose={this.hide}
+					>
+						<ChartIndicatorConfigurationForm />
+					</Modal>
+				) : null}
+			</>
 		);
 	}
 }
